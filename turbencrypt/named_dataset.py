05f02ad5f0f08b8911f9a6f8a35cb676f01c526a
Anthony Bao <abao@utexas.edu>
2025-02-13 13:39:38 -0600

    stupid changes

--- turbencrypt/named_dataset.py
+++ turbencrypt/named_dataset.py
@@ -19,7 +19,7 @@ class NamedTensorDataset(torch.utils.data.Dataset):
         return len(self.X)
     
     def __getitem__(self, idx):
-        x_sample = self.X[idx].unsqueeze(0)  # Add channel dimension
-        y_sample = self.y[idx].permute(2, 0, 1)  # Change from [256, 256, 2] to [2, 256, 256]
+        x_sample = self.X[idx]  # Add channel dimension
+        y_sample = self.y[idx]  # Change from [256, 256, 2] to [2, 256, 256]
 #         print(f"Input x shape: {x_sample.shape}, Target y shape: {y_sample.shape}")
         return {"x": x_sample, "y": y_sample}
