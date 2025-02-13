22b6a91f4c483a7a510b9429e7534ce773246e04
Anthony Bao <abao@utexas.edu>
2025-02-13 16:30:17 -0600

    got the stupid thing working

--- scripts/stupid.py
+++ scripts/stupid.py
@@ -8,8 +8,7 @@ which makes it easy to checkpoint and resume training states.
 
 """
 
-# %%
-# 
+
 import torch
 import matplotlib.pyplot as plt
 import sys
@@ -19,20 +18,22 @@ from neuralop.training import AdamW
 from neuralop.data.datasets import load_darcy_flow_small
 from neuralop.utils import count_model_params
 from neuralop import LpLoss, H1Loss
+from turbencrypt.FNO import FourierNO
 
 device = 'cpu'
+data_path = "/Users/gilpinlab/turbulence_encryption/data/forreal.npz"
+random_state = 42
+test_size = 0.2
 
-
-# %%
 # Loading the Navier-Stokes dataset in 128x128 resolution
 train_loader, test_loaders, data_processor = load_darcy_flow_small(
         n_train=1000, batch_size=32, 
         test_resolutions=[16, 32], n_tests=[100, 50],
         test_batch_sizes=[32, 32],
 )
+train_loader2, test_loaders = FourierNO.makeFNO(data_path, random_state, test_size)
 
 
-# %%
 # We create an FNO model
 
 model = FNO(n_modes=(16, 16),
@@ -50,7 +51,6 @@ print(f'\nOur model has {n_params} parameters.')
 sys.stdout.flush()
 
 
-# %%
 #Create the optimizer
 optimizer = AdamW(model.parameters(), 
                                 lr=8e-3, 
@@ -58,12 +58,12 @@ optimizer = AdamW(model.parameters(),
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=30)
 
 
-# %%
+#
 # Creating the losses
 l2loss = LpLoss(d=2, p=2)
 h1loss = H1Loss(d=2)
 
-train_loss = h1loss
+train_loss = l2loss
 eval_losses={'h1': h1loss, 'l2': l2loss}
 
 
@@ -84,7 +84,7 @@ trainer = Trainer(model=model, n_epochs=20,
                   use_distributed=False,
                   verbose=True)
 
-trainer.train(train_loader=train_loader,
+trainer.train(train_loader=train_loader2,
               test_loaders={},
               optimizer=optimizer,
               scheduler=scheduler, 
