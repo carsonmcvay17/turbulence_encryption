0729ee9da3a44ae17c1a640f7354742fa814633b
Anthony Bao <abao@utexas.edu>
2025-02-13 13:36:58 -0600

    added incremental trainer

--- scripts/runFNO.py
+++ scripts/runFNO.py
@@ -5,53 +5,85 @@ from neuralop import Trainer
 from neuralop.training import AdamW
 from neuralop import LpLoss, H1Loss
 from turbencrypt.FNO import FourierNO
+from neuralop.data.transforms.data_processors import IncrementalDataProcessor
+from neuralop.training.incremental import IncrementalFNOTrainer
+
 
 
 # from Training import Trainer2
 
-data_path = "/Users/gilpinlab/turbulence_encryption/data.npz"
+data_path = "/Users/gilpinlab/turbulence_encryption/data/forreal.npz"
 random_state = 42
-test_size = .2
+test_size = 0.2
 device = 'cpu'
-loaders = FourierNO.makeFNO(data_path, random_state, test_size)
-train_loader = loaders[0]
-test_loader = loaders[1]
+train_loader, test_loader, data_processor = FourierNO.makeFNO(data_path, random_state, test_size)
 
 
-model = FNO(n_modes=(16,16),
+model = FNO(
+    max_n_modes=(64,64),
+    n_modes=(2,2),
            in_channels=1,
-           out_channels=1, 
+           out_channels=2, 
            hidden_channels=32,
-           projection_channel_ratio=2)
+           )
 
 model = model.to(device)
 
+
+data_transform = IncrementalDataProcessor(
+    in_normalizer=data_processor.in_normalizer,
+    out_normalizer=data_processor.out_normalizer,
+    device=device,
+    subsampling_rates=[4, 2, 1],
+    dataset_resolution=64,
+    dataset_indices=[2, 3],
+    epoch_gap=200,
+    verbose=True,
+)
+
+data_transform = data_transform.to(device)
+
+
 optimizer = AdamW(model.parameters(),
-                 lr=1e-4,
-                 weight_decay=1e-4)
+                 lr=8e-3, weight_decay=1e-3) 
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=30)
+# scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda epoch: 0.0)
 
 # create losses
 l2loss = LpLoss(d=2, p=2)
 h1loss = H1Loss(d=2)
 
 train_loss = h1loss
-eval_losses={'h1': h1loss, 'l2': l2loss}
+eval_losses={'l2': l2loss, 'h1': h1loss}
+
 
 # create the trainer
-trainer = Trainer(model=model, n_epochs=20, 
-                 wandb_log=False,
-                 device=device,
-                 mixed_precision=False,
-                 data_processor=None,
-                 eval_interval=3,
-                 log_output=False,
-                 use_distributed=False,
-                 verbose=True)
+# trainer = Trainer(model=model, n_epochs=20, 
+#                  wandb_log=False,
+#                  device=device,
+#                  data_processor=data_processor,
+#                  eval_interval=1,
+#                  log_output=False,
+#                  use_distributed=False,
+#                  verbose=True)
+trainer = IncrementalFNOTrainer(
+    model=model,
+    n_epochs=18,
+    data_processor=data_transform,
+    device=device,
+    verbose=True,
+    incremental_loss_gap=False,
+    incremental_grad=True,
+    incremental_grad_eps=0.9999,
+    incremental_loss_eps = 0.001,
+    incremental_buffer=5,
+    incremental_max_iter=18,
+    incremental_grad_max_iter=2,
+)
 
 # train model on data
 trainer.train(train_loader=train_loader, 
-             test_loaders={"default": test_loader},
+             test_loaders={"problem": test_loader},
              optimizer=optimizer,
              scheduler=scheduler,
              regularizer=False,
