f338f015c8524613c82464ce33ff204c2ef684cf
Anthony Bao <abao@utexas.edu>
2025-03-18 17:31:31 -0500

    losing track of changes it's probably the same

--- turbencrypt/make_movie.py
+++ turbencrypt/make_movie.py
@@ -4,9 +4,14 @@ from typing import Any
 
 import glob
 from tqdm import tqdm
+import matplotlib.image as mpi
+from skimage.transform import resize
+from jax_cfd.base.forcings import kolmogorov_forcing
 
 from turbencrypt.run_turbulence import Turbulence
 from turbencrypt.make_forcing import Forcings
+from turbencrypt.make_dataset import Dataset
+from turbencrypt.make_forcing import Forcings, FourierTransform
 
 
 class movie():
@@ -14,27 +19,36 @@ class movie():
     class designed to create a movie of the turbulence simulations
     """
 
-    def make_movie(self, image_dir: str, save_path: str, config: dict[str, Any], visualize_idx: int | None = None):
+    def make_movie(self, image_dir: str, save_path: str, sim_config: dict[str, Any], visualize_idx: int | None = None):
         """
         make movie
         """
         
-
-        # define forcing images
-        image_paths = glob.glob(f"{image_dir}/*")
+        # run simulation
         image_path = image_dir
+        fftmodel = FourierTransform()
+        img = fftmodel.load_image(image_path) 
+        # breakpoint()
+        # forcing_fn = lambda grid: Forcings().mod_kolmogorov_forcing(img, grid)
+        forcing_fn = lambda grid: kolmogorov_forcing(grid, k=6)
+        model = Turbulence(**sim_config)
+        vorticity, forcing_x_array, forcing_y_array = model.run_turbulence(forcing_fn, movie=True)
+        # define forcing images
+        # image_paths = glob.glob(f"{image_dir}/*")
+    
 
 
-        for idx, img_path in tqdm(enumerate(image_path), desc="Running sims...", total=len(image_path)):
+        # for idx, img_path in tqdm(enumerate(image_paths), desc="Running sims...", total=len(image_paths)):
 
-            # define forcing function
-            wave_number = 1
-            offsets = ((0, 0), (0, 0))
-            forcing_fun = lambda grid: Forcings().mod_kolmogorov_forcing(image_path, grid, k=wave_number, offsets=offsets)
+            
 
-            # run simulation
-            model = Turbulence(**config)
-            vorticity, forcing_x_array, forcing_y_array = model.run_turbulence(forcing_fun, movie=True)
+        #     # run simulation
+        #     model = Dataset()
+        #     model.run_sim_from_images(
+        #         image_dir=image_paths,
+        #         save_path=save_path,
+        #         config=sim_config
+        #     )
             
             
 
